db17f03001aba65574ad66853cde60779a8ad8e6
srmarisko <sr.evm.mar@gmail.com>
2022-04-26 18:27:20 +0200

    nuevo src con css

--- src/app/libros.ts
+++ src/app/libros.ts
@@ -27,7 +27,7 @@ export let libros = [
         comentarios: ["Carlos Castrejón:\n Muy buen libro.", "Ricardo Martín:\n Me ha gustado mucho, lo recomiendo."]
     },
     {
-        id: 0,
+        id: 1,
         titulo: "Harry Potter y la Cámara Secreta",
         autor: "J. K. Rowling",
         ISBN: "9788478884957",
@@ -37,6 +37,19 @@ export let libros = [
         editorial: "salamandra",
         precio: 15.95,
         sinopsis: "El verano de Harry Potter ha incluido el peor cumpleaños de su vida, las funestas advertencias de un elfo doméstico llamado Dobby y el rescate de casa de los Dursley protagonizado por su amigo Ron Weasley al volante de un coche mágico volador. De vuelta en el Colegio Hogwarts de Magia y Hechicería, donde va a empezar su segundo curso, Harry oye unos extraños susurros que resuenan por los pasillos vacíos. Y entonces empiezan los ataques y varios alumnos aparecen petrificados... Por lo visto, las siniestras predicciones de Dobby se están cumpliendo...",
+        comentarios: ["Gonzalo Mirillas:\n Muy buen libro", "Juan Pablo García:\n Me ha gustado mucho, lo recomiendo."]
+    },
+    {
+        id: 2,
+        titulo: "Mi portada de libro",
+        autor: "H. P. Mowli",
+        ISBN: "9788478884955",
+        edicion: 10,
+        ano:2022,
+        idioma: "Español",
+        editorial: "salamandra",
+        precio: 20.87,
+        sinopsis: "Mi libro Mi libro Mi libro Mi libro Mi libro Mi libro Mi libro Mi libro Mi libro Mi libro Mi libro Mi libro ",
         comentarios: ["Gonzalo Mirillas:\n Muy buen libro", "Juan Pablo García:\n Me ha gustado mucho, lo recomiendo."]
     }
 ]
